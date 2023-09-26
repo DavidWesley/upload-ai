bb53ebd21726ec90a5cd04b2833231088bbc4e6b
David Wesley <contato.dwesley@outlook.com>
2023-09-26 16:50:06 -0300

    fix(server): Fix blob file handling with `toFile` method
    Fix blob file handling in openai's transcriptions requests
    just by adding the `toFile` method from the openai API over the blob data

--- server/src/routes/create-transcription.ts
+++ server/src/routes/create-transcription.ts
@@ -1,15 +1,11 @@
 import { FastifyInstance } from "fastify"
-import fs from "node:fs"
-import path from "node:path"
-import stream from "node:stream"
-import url from "node:url"
 
 import { openai } from "@/lib/openai"
 import { prisma } from "@/lib/prisma"
 import { downloadFileStream } from "@/lib/storage"
 import { createTranscriptionBodySchema, createTranscriptionParamsSchema } from "@/schemas/zod"
 import { StatusCodes } from "@/utils/constants/http-status-code"
-import { TimeUnits } from "@/utils/constants/time-units"
+import { toFile } from "openai"
 
 export async function createTranscriptionRoute(app: FastifyInstance) {
   app.post("/videos/:videoID/transcription", async (req, res) => {
@@ -33,26 +29,20 @@ export async function createTranscriptionRoute(app: FastifyInstance) {
       },
     })
 
-    // const audioDownloadPath = getFilePublicURL("nlw-ia-audio-storage", video.path, { download: true })
     const audioBlob = await downloadFileStream("nlw-ia-audio-storage", video.path)
 
     if (!audioBlob.data) {
       return res.status(StatusCodes.FAILED_DEPENDENCY).send({ error: "Uncovered error" })
     }
 
-    const __filename = url.fileURLToPath(import.meta.url)
-    const __dirname = path.dirname(__filename)
-    const audioLocalPath = path.resolve(__dirname, "../../tmp/audios", `audio-${videoID}.mp3`)
-
-    await stream.Readable.fromWeb(audioBlob.data.stream()).pipe(
-      fs.createWriteStream(audioLocalPath, {
-        flags: "w+",
-      })
-    )
+    const file = await toFile(audioBlob.data, video.name, {
+      lastModified: Date.parse(video.createdAt.toISOString()),
+      type: "audio/mpeg",
+    })
 
     const result = await openai.audio.transcriptions.create(
       {
-        file: fs.createReadStream(audioLocalPath, { flags: "r" }),
+        file: file,
         model: "whisper-1",
         language: "pt",
         response_format: "json",
@@ -68,7 +58,10 @@ export async function createTranscriptionRoute(app: FastifyInstance) {
     )
 
     const transcription = result.text
-    if (transcription) {
+    if (!transcription) {
+      return res.status(StatusCodes.EXPECTATION_FAILED).send({ error: "Não foi possível gerar uma transcrição" })
+    }
+
       await prisma.video.update({
         where: {
           id: videoID,
@@ -77,17 +70,7 @@ export async function createTranscriptionRoute(app: FastifyInstance) {
           transcription,
         },
       })
-    } else {
-      res.status(StatusCodes.EXPECTATION_FAILED).send({ error: "Não foi possível gerar uma transcrição" })
-    }
 
-    fs.promises.rm(audioLocalPath, {
-      force: true,
-      recursive: true,
-      retryDelay: TimeUnits.CONSTANTS.second * 5,
-      maxRetries: 4,
-    })
-
-    return res.status(StatusCodes.CREATED).send(transcription ?? "Não foi possível gerar uma transcrição")
+    return res.status(StatusCodes.CREATED).send(transcription)
   })
 }
